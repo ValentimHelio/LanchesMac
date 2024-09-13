17c4e84c0aaf53878de11880a5c01488f0ab7374
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-09-13 16:12:03 -0400

    18. Configurando o EF Core

---
+++ Context/AppDBbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context
+{
+    public class AppDBbContext : DbContext
+    {
+        public AppDBbContext(DbContextOptions<AppDBbContext> options) : base(options) { }
+        
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+            
+       
+    }
+}
