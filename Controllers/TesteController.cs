53f85943ca84ab051834cf35ad677a57e3b3349d
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-09-13 14:11:52 -0400

    Seção 2

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
