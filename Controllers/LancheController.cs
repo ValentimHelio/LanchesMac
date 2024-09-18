41a728dee16a1db3fe570140d529ba9bbde40d78
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-09-18 14:42:29 -0400

    32. Criando o Controller a View para exibir os lanches

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILanchesRepository _branchesRepository;
+
+        public LancheController(ILanchesRepository branchesRepository)
+        {
+            _branchesRepository = branchesRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _branchesRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +17,8 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddDbContext<AppDBbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-        services.AddTransient<ILanchesRepository, ILanchesRepository>();
-        services.AddTransient<ICategoriasRepository, ICategoriasRepository>();
+        services.AddTransient<ILanchesRepository, LancheRepository>();
+        services.AddTransient<ICategoriasRepository, CategoriaRepository>();
         services.AddControllersWithViews();
     }
 
