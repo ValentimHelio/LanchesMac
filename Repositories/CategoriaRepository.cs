68546763349d605b433f2d56e5242bbe2eb7a69f
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-09-18 09:52:07 -0400

    29.  Implementand o padrão Repository para Categoria

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class CategoriaRepository : ICategoriasRepository
+    {
+        public readonly AppDBbContext _context;
+
+        public CategoriaRepository(AppDBbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => throw new NotImplementedException();
+    }
+}
---
+++ Repositories/Interfaces/ICategoriasRepository.cs
@@ -0,0 +1,9 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ICategoriasRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
