ab9e932e175c3a1ac5d3ffd427ed852f3621ebed
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-09-18 10:06:06 -0400

    30. Implementando o padrão Repository para Lanches

---
+++ Repositories/LancheRepository.cs
@@ -0,0 +1,28 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILanchesRepository
+    {
+        public readonly AppDBbContext _context;
+
+        public LancheRepository(AppDBbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+                                                                .Where(l => l.IsLanchePreferido)
+                                                                .Include(c => c.Categoria);
+
+        public Lanche GetLancheById(int id)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == id);
+        }
+    }
+}
