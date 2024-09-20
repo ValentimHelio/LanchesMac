f9b0f854b85b75f25fd3acaad1a42b1db3356e07
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-09-20 09:10:41 -0400

    46. Adicionar Itens ao Carrinho de Compras

--- Models/CarrinhoCompra.cs
+++ Models/CarrinhoCompra.cs
@@ -33,5 +33,26 @@ namespace LanchesMac.Models
                 CarrinhoCompraId = carrinhoId
             };
         }
+
+        public void AdicionarAoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(s => s.Lanche.LancheId == lanche.LancheId && s.CarrinhoCompraId == CarrinhoCompraId);
+
+            if (carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+        }
     }
 }
