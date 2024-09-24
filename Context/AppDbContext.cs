01136483db58a78089e034a7df47240b823a42a0
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-09-24 09:47:24 -0400

    63. Pedidos - Expandindo o modelo de domínio

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -6,12 +6,12 @@ namespace LanchesMac.Context
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        
+
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidosDetalhe { get; set; }
 
-            
-       
     }
 }
