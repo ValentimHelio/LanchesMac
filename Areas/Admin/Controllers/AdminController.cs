eb9d4bc2dde93a90dae01fe6471b03191e6e1e30
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-10-01 14:29:43 -0400

    82. Roteiro para implementar o perfil para o usuário Admin

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
         [Area("Admin")]
+        [Authorize]
         public IActionResult Index()
         {
             return View();
