3a1449acf5e5899031b81960e166899c4dd5c9d6
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-10-03 15:12:19 -0400

    87. Criar os controladores e views para gerenciar lanches, categorias e pedidos - II

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -6,7 +6,7 @@ namespace LanchesMac.Areas.Admin.Controllers
     public class AdminController : Controller
     {
         [Area("Admin")]
-        [Authorize("Admin")]
+        [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             return View();
