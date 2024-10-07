581092f9557c045fabee532edad713e0b5165340
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-10-07 15:57:08 -0400

    93. Criando o relatório de pedidos por período

---
+++ Areas/Admin/Services/RelatorioVendasServices.cs
@@ -0,0 +1,38 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Areas.Admin.Services
+{
+    public class RelatorioVendasServices
+    {
+        private readonly AppDbContext _context;
+
+        public RelatorioVendasServices(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Pedido>> FindByDateAsync(DateTime? mimDate, DateTime? maxDate)
+        {
+            var resultado = from obj in _context.Pedidos select obj;
+
+            if (mimDate.HasValue)
+            {
+                resultado = resultado.Where(x => x.PedidoEnviado >= mimDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                resultado = resultado.Where(x => x.PedidoEnviado <= maxDate.Value);
+            }
+
+            return await resultado
+                        .Include(l => l.PedidoItens)
+                        .ThenInclude(l => l.Lanche)
+                        .OrderByDescending(x => x.PedidoEnviado)
+                        .ToListAsync();
+
+        }
+    }
+}
