dc90746673e70f847938167a7754692278d82ed8
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-10-11 15:56:13 -0400

    99. Gerando gráficos usando a biblioteca GoogleChart - Criando o modelo de dominio.

---
+++ Models/LancheGrafico.cs
@@ -0,0 +1,9 @@
+﻿namespace LanchesMac.Models
+{
+    public class LancheGrafico
+    {
+        public string LancheNome { get; set; }
+        public int LancheQuantidade { get; set; }
+        public decimal LamcheValorTotal { get; set; }
+    }
+}
