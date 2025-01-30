87546b97bbd7878bfb2ed1f179778f553c704fd0
Valentim Helio de Oliveira <timvale@hotmail.com>
2025-01-30 10:54:38 -0400

    16. Criando o Modelo de dominio

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaName { get; set; }
+        public string CategoriaDescricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+    }
+}
